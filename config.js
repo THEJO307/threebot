da1a2afa9ebab02c907027327cc060394cb6107c
ranggaslebeww <117437212+ranggaslebeww@users.noreply.github.com>
2022-11-16 14:09:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','6285731811079','6285339388512']
-global.premium = ['60177426853']
+global.owner = ['62895801187592','','62895801187592']
+global.premium = ['62895801187592']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
