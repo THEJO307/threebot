b3bb262d66163da2d4c2f13221230104e2f09d07
Boylon <116648641+Boylon@users.noreply.github.com>
2022-11-21 13:50:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THREE BOT'
+global.ownername = 'Satria Ofc'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','6285731811079','6285339388512']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281779045019','6281779045019','6281779045019']
+global.premium = ['6281779045019']
+global.packname = 'Zenn Botzz'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
